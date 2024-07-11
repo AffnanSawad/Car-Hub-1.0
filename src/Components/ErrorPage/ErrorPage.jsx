242754497bd5df470992a7ef00033ea236ee59f2
Affnan Sawad <affnansawad2002@gmail.com>
2024-07-11 22:29:32 +0600

    Navbar Added with 4 navigation options

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+
+const ErrorPage = () => {
+
+
+    return (
+        <div>
+             
+        
+        <h1>OPPS!Page Not Found ! Please Go Back.</h1>
+
+        <br />
+
+        <Link to={'/'} >
+         
+
+        <button className="btn btn-error">Error</button>
+         
+        </Link>
+
+
+
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+
+//    Nav Links
+
+ const NavLinks = <>
+ 
+    <li className="font-bold"> <Link to='/'> Home   </Link>   </li>
+    <li className="font-bold"> <Link to='/cart'> My Cart   </Link>   </li>
+    <li className="font-bold"> <Link to='/booking'> Booking List   </Link>   </li>
+    <li className="font-bold"> <Link to='/stat'> Statistics   </Link>   </li>
+ 
+ 
+ </>
+
+
+
+
+    return (
+        <div>
+           <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+         
+    {NavLinks}
+
+
+
+      </ul>
+    </div>
+    <a className=" text-2xl font-bold">Car-Hub-1.0</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+                
+
+
+      {NavLinks}
+
+          
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn btn-outline btn-accent">Log In</a>
+  </div>
+</div> 
+        </div>
+    );
+};
+
+export default Navbar;
+    
+        
