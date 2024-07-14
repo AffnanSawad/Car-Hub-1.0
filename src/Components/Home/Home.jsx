1b0cac7b67f9423a245014d5d194d581c919f6c1
Affnan Sawad <affnansawad2002@gmail.com>
2024-07-15 02:46:16 +0600

    Marquee js added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,6 @@
 import Banner from "../Banner/Banner";
+import MarqueeDesign from "../MarqueDesign/MarqueeDesign";
+
 import MyCars from "../MyCars/MyCars";
 
 
@@ -9,6 +11,21 @@ const Home = () => {
               
     <Banner></Banner>
 
+    <div className="mt-20 mb-10">
+
+        <h1 className="banner2 text-center mb-20 text-4xl font-extrabold 
+    
+      text-red-600
+        
+        
+        ">  Brand's You May Know ! </h1>
+
+<MarqueeDesign ></MarqueeDesign>
+
+        </div>
+
+    
+ 
     <MyCars></MyCars>
               
 
---
+++ src/Components/MarqueDesign/MarqueeDesign.jsx
@@ -0,0 +1,48 @@
+
+import Marquee from "react-fast-marquee";
+const MarqueeDesign = () => {
+    return (
+        <div className="mt-10">
+            
+       
+            <Marquee className="mt-10 w-1/2"
+            
+            speed={500} pauseOnHover={true}
+            
+            >
+          
+   <img className="w-1/4 mr-64" src="https://i.ibb.co/mbz7bXb/BMW-svg.png" alt="BMW-svg" />
+
+   <img className="mr-64" src="https://i.ibb.co/BVS76sP/images.jpg" alt="BMW-svg" />
+   
+   <img className=" mr-64" src="https://i.ibb.co/JrPvpZP/images.png" alt="BMW-svg" />
+
+   <img className="w-1/4 mr-64" src="https://i.ibb.co/QMW1Nz1/ferrari-brand-logo-car-symbol-design-italian-auto-vector-46018659.jpg" alt="BMW-svg" />
+
+
+   <img className=" mr-64" src="https://i.ibb.co/Z8sh7Sy/images-2.png" alt="BMW-svg" />
+
+
+
+   <img className="w-1/2  mr-32" src="https://i.ibb.co/qyVBznR/images-1.png" alt="BMW-svg" />
+
+
+   <img className="w-1/2  mr-32" src="https://i.ibb.co/8PJm5Sf/Porsche-Logo.jpg" alt="BMW-svg" />
+
+
+
+   <img className="w-1/2  mr-32" src="https://i.ibb.co/3YzzYyM/ford-logo-transparent-background-vector-illustration-motor-company-american-multinational-automobile.webp" alt="BMW-svg" />
+
+
+   
+
+
+
+</Marquee>
+
+
+        </div>
+    );
+};
+
+export default MarqueeDesign;
